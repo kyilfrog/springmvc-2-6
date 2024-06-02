748f9c4778f3df66771596dbbd25d7cfa7133c8d
kyilfrog <160857010+kyilfrog@users.noreply.github.com>
2024-06-02 16:52:46 +0900

    @ExceptionHandler

--- src/main/java/hello/exception/api/ApiExceptionV3Controller.java
+++ src/main/java/hello/exception/api/ApiExceptionV3Controller.java
@@ -1,23 +1,25 @@
 package hello.exception.api;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
-import hello.exception.exception.BadRequestException;
+import hello.exception.api.ApiExceptionController.MemberDto;
 import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RestController          /* API통신이기 때문에*/
-public class ApiExceptionController {
+@RestController
+public class ApiExceptionV3Controller {
 	
-	@GetMapping("/api/members/{id}")
+	@GetMapping("/api3/members/{id}")
 	public MemberDto getMember(@PathVariable("id") String id) {
 		
 		if(id.equals("ex")) {
@@ -35,43 +37,12 @@ public class ApiExceptionController {
 		return new MemberDto(id, "hello " + id);
 	}
 	
-	@GetMapping("/api/response-status-ex1")
-	public String responseStatusEx1() {
-		throw new BadRequestException();
-	}
-	
-	@GetMapping("/api/response-status-ex2")
-	public String responseStatusEx2() {
-		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
-	}
-	
-	@GetMapping("/api/default-handler-ex")
-	public String defaultException(@RequestParam Integer data) {
-		return "ok";
-	}
-	
 	@Data
 	@AllArgsConstructor
 	static class MemberDto{
 		private String memberId;
 		private String name;
 	}
+	
+	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
